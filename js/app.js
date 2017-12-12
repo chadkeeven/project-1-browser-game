5aff907bfeb34eff6686ea5ea48ded6b388d27cc
chadkeeven <chadkeeven@gmail.com>
2017-12-12 16:10:17 -0700

    added clock function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
 	console.log("js connected!");
+	var total_time = 0;
+	setInterval(function(){
+		console.log(total_time);
+		total_time += 1;
+		$(".clock").html(total_time);
+	},1000);
 });
