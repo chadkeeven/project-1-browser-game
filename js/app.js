689d620153f01fba2751d9609114660e5782f422
chadkeeven <chadkeeven@gmail.com>
2017-12-12 20:08:21 -0700

    added stoplight design and function to switch

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
 $(document).ready(function() {
-	console.log("js connected!");
+	//Clock
 	var total_time = 0;
 	setInterval(function(){
-		console.log(total_time);
+		//console.log(total_time);
 		total_time += 1;
 		$(".clock").html(total_time);
 	},1000);
+	//Stoplight
+	/*
+	To Do:
+	1)Get setInterval to change random stopLightRan
+	instead of using the same one.
+	2)instead of hide change color to black or grey.
+	*/
+	$('.greenLight').hide();
+
+	let stopLightRan = Math.random() * (3000 - 1000) + 1000;
+		setInterval(function(){
+				console.log(stopLightRan);
+				if ($('.greenLight').is(':visible')) {
+					$('.redLight').show();
+					$('.greenLight').hide();
+				}else if($('.greenLight').is(':visible') === false){
+					$('.greenLight').show();
+					$('.redLight').hide();
+				}	
+		},stopLightRan);
+
 });
