5887f8c407f95e491554b2fbb24740fee68b5ea8
chadkeeven <chadkeeven@gmail.com>
2017-12-14 15:56:27 -0700

    fixed stoplight logic to be random

--- js/app.js
+++ js/app.js
@@ -3,46 +3,78 @@ $(document).ready(function() {
 	var total_time = 0;
 	setInterval(function(){
 		//console.log(total_time);
-		total_time += 1;
+		total_time += 0.01;
+		total_time = Math.round(100 * total_time)/100;
 		$(".clock").html(total_time);
-	},1000);
+	},10);
 	//Stoplight
 	/*
 	To Do:
-	1)Get setInterval to change random stopLightRan
-	instead of using the same one.
-	2)instead of hide change color to black or grey.
+	-instead of hide change color to black or grey.
 	*/
 	$('.greenLight').hide();
 
-	let stopLightRan = Math.random() * (3000 - 1000) + 1000;
+	
+
+	var switchPending = false;
 	setInterval(function(){
-				//console.log(stopLightRan);
-				if ($('.greenLight').is(':visible')) {
-					$('.redLight').show();
-					$('.greenLight').hide();
-				}else if($('.greenLight').is(':visible') === false){
-					$('.greenLight').show();
-					$('.redLight').hide();
-				}	
-			},stopLightRan);
+		if (!isWinner()) {
+			let  stopLightRan = Math.random() * (3000 - 1000) + 1000;
+			if (!switchPending) {
+				console.log(stopLightRan);
+				setTimeout(function(){
+					if ($('.greenLight').is(':visible')) {
+						$('.redLight').show();
+						$('.greenLight').hide();
+					}else if($('.greenLight').is(':visible') === false){
+						$('.greenLight').show();
+						$('.redLight').hide();
+					}
+					switchPending = false;		
+				},stopLightRan);
+			}
+			switchPending = true;
+		}
+	},50);
 
 	//Move user piece
 
 	var currentMargin = 0;
+
 	$(this).keyup(function(event) {
-		console.log($('.greenLight').is(':visible') === true);
-		if ($('.greenLight').is(':visible') === true) {
-			console.log("light is green");
-			currentMargin += 5;
-			$('.user_piece').css('margin-left', currentMargin + '%');
-		}else if($('.greenLight').is(':visible') === false){
-			if ($('.redLight').is(':visible') && currentMargin > 0){
-				console.log("light is red");
-				currentMargin -= 5;
+		if (isWinner() == false) {
+			if ($('.greenLight').is(':visible') === true) {
+				currentMargin += 5;
 				$('.user_piece').css('margin-left', currentMargin + '%');
+				isWinner();
+			}else if($('.greenLight').is(':visible') === false){
+				if ($('.redLight').is(':visible') && currentMargin > 0){
+					currentMargin -= 5;
+					$('.user_piece').css('margin-left', currentMargin + '%');
+				}
 			}
+		}else{
+			alert("Game is over!");
 		}
-		console.log(currentMargin);
 	});
+
+	//Winning Condition
+	function isWinner(){
+		if (currentMargin === 100) {
+			$('#highScoreModal').show();
+			total_time = 0;
+			currentMargin = 0;
+			$('.user_piece').css('margin-left', currentMargin + '%');
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	//x button on highscore modal
+	$('.xButton').click(function(event) {
+		$('.modal').hide();
+	});
+
 });
